515ce0efd9d2c997e7cb3278abd691a3e9d58a89
Milena Cordeiro <mcs9823@gmail.com>
2023-05-12 17:49:59 -0300

    Requisito 2 - implementando componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { userEmail } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <header>
+          <h1>WALLET</h1>
+          <p data-testid="email-field">
+            Email:
+            {userEmail}
+          </p>
+          <p data-testid="total-field">
+            Despesa Total: 0
+            {' '}
+            <span data-testid="header-currency-field">BRL</span>
+          </p>
+        </header>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
