10e774e2a6009f2d51679a6896a2ed6ba0f8f3c8
Milena Cordeiro <mcs9823@gmail.com>
2023-05-12 17:12:48 -0300

    iniciando a implementação do store, actions e reducers para possibilitar o save do email na page Login no global state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import isEmail from 'validator/lib/isEmail';
 
 class Login extends React.Component {
   state = {
     isDisabled: true,
+    email: '',
+    password: '',
+  };
+
+  validateFields = () => {
+    const { email, password } = this.state;
+
+    const min = 6;
+    const verifyEmail = isEmail(email);
+    const verifyPassWord = password.length >= min;
+
+    this.setState({
+      isDisabled: !(verifyEmail && verifyPassWord),
+    });
   };
 
   handleChange = ({ target }) => {
     const { type, value } = target;
-    // console.log(value);
-    const min = 6;
-    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    const verifyEmail = type === 'email' && value.match(regex);
-    const verifyPassWord = type === 'password' && value >= min;
 
-    if (verifyEmail && verifyPassWord) {
-      this.setState((prevState) => ({
-        isDisabled: !prevState.state,
-      }));
-    }
+    this.setState({
+      [type]: value,
+    }, this.validateFields);
   };
 
   render() {
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_EMAIL } from '../actions';
+
+const INITIAL_STATE_USER = {
+  email: '', // string que armazena o email da pessoa usuária
+};
+
+const userReducer = (state = INITIAL_STATE_USER, action) => {
+  switch (action.type) {
+  case ADD_EMAIL:
+
+    return {
+      ...state,
+      email: action.email,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
