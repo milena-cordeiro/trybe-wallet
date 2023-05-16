8fcb93629431bfaafafce70f181de0b5935a4e6e
Milena Cordeiro <mcs9823@gmail.com>
2023-05-16 15:26:10 -0300

    Requisito 5 - alterando arquivos dos testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouterAndRedux from './renderWithRouterAndRedux';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
 import { App } from '../App';
 
 describe('Testa a renderização da página de Login e seus elementos', () => {
