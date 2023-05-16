ee2fb974940f24d5d81639d7479b4887151a778a
Milena Cordeiro <mcs9823@gmail.com>
2023-05-16 15:40:28 -0300

    Requisito 6 - desenvolvendo o inicio da tabela de gastos

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -8,7 +9,12 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <br />
-        <WalletForm />
+        <fieldset>
+          <legend>Adicione despesas</legend>
+          <WalletForm />
+        </fieldset>
+        <br />
+        <Table />
       </div>
     );
   }
